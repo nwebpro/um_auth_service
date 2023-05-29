40990cc9c803a73cdd70c2591f6a253eaeca3f6a
nwebpro <nwebpro.work@gmail.com>
2023-05-29 23:56:09 +0600

    [DONE] create user service

---
+++ src/app/modules/Users/Users.utils.ts
@@ -0,0 +1,17 @@
+import { User } from './Users.model'
+
+// Get last user id from database
+export const findLastUserId = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean()
+  return lastUser?.id
+}
+
+// Generate user id
+export const generateUserId = async () => {
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
+  // increment id by 1
+  const incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementId
+}
