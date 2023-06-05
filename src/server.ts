2d286494ac59539f3b91dc3befe15762ecb9d5bf
nwebpro <nwebpro.work@gmail.com>
2023-06-05 22:34:16 +0600

    [DONE] Extra layer validation with Zod

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
-import { errorLogger, infoLogger } from "./shared/logger";
-import { Server } from "http";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorLogger, infoLogger } from './shared/logger';
+import { Server } from 'http';
 
 // Uncaught expression error handler
-process.on("uncaughtException", (error) => {
-  errorLogger.error("Uncaught Exception is detected", error);
+process.on('uncaughtException', error => {
+  errorLogger.error('Uncaught Exception is detected', error);
   process.exit(1);
 });
 let server: Server;
@@ -15,19 +15,19 @@ let server: Server;
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    infoLogger.info("Database connected");
+    infoLogger.info('Database connected');
     server = app.listen(config.port, () => {
       infoLogger.info(`Server started at http://localhost:${config.port}`);
     });
   } catch (error) {
-    errorLogger.error("Database connection failed", error);
+    errorLogger.error('Database connection failed', error);
   }
 
   //  Fix Error Log , Gracefully off your server
-  process.on("unhandledRejection", (error) => {
+  process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error("Unhandled Rejection is detected", error);
+        errorLogger.error('Unhandled Rejection is detected', error);
         process.exit(1);
       });
     } else {
@@ -38,8 +38,8 @@ async function bootstrap() {
 bootstrap();
 
 // Gracefully off your server
-process.on("SIGTERM", () => {
-  infoLogger.info("SIGTERM RECEIVED. Shutting down gracefully");
+process.on('SIGTERM', () => {
+  infoLogger.info('SIGTERM RECEIVED. Shutting down gracefully');
   if (server) {
     server.close();
   }
